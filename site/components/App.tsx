d55f9ae67bf899c84201d1b38cf7601538f2bfc5
Saulo Oliveira <sauloeduardo@gmail.com>
2018-10-27 11:49:30 +0100

    Enable Material-UI Support

--- site/components/App.tsx
+++ site/components/App.tsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+// import 'typeface-roboto';
+import Typography from '@material-ui/core/Typography';
+
+import SvgIcon from '@material-ui/core/SvgIcon';
+function HomeIcon(props: any) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+}
+
+import Icon from '@material-ui/core/Icon';
+
 const App = class extends Component {
   render() {
-    return <h1>Hello React App!</h1>;
+    return <div>
+      <HomeIcon />
+      <Icon color="primary">
+        add_circle
+      </Icon>
+      <Typography variant="h1">Hello React App!</Typography>
+    </div>;
   }
 }
 
